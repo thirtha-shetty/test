aefdfc4926f56bb8e53d332a97dc72535de31dac
thirtha-shetty <thirthashetty93@gmail.com>
2025-11-08 23:50:57 +0530

    Create Java service with main and add methods

---
+++ services/java-service/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Java Service is running!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
